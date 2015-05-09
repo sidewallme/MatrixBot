849f46c59510f68b6a2a9a43d903a459fe482686
JX <sidewallme@gmail.com>
2015-05-09 17:07:09 -0500

    done

--- src/matrixbot/PageMaker.java
+++ src/matrixbot/PageMaker.java
@@ -12,7 +12,10 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import static jdk.nashorn.internal.objects.NativeRegExp.source;
 
 /**
  *
@@ -23,25 +26,105 @@ import java.util.List;
  * @author erichsu
  */
 public class PageMaker {
-    public static void main() throws FileNotFoundException, UnsupportedEncodingException, IOException{
+        public static ArrayList<Integer> ids = new ArrayList<>();
+        public static ArrayList<Double> xs = new ArrayList<>();
+        public static ArrayList<Double> ys = new ArrayList<>();
+        public static ArrayList<Integer> clus = new ArrayList<>();
+        
+        public static Map<Integer, Double> idToX = new HashMap<>();
+        public static Map<Integer, Double> idToY = new HashMap<>();
+        public static Map<Integer, Integer> idToClus = new HashMap<>();
+
+        
+    public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException, IOException{
+        readGephi();
+        init();
+        extract();
+        
+    }
+    
+    public static void init(){
+        
+        for(int i=0;i<270;i++){
+            idToX.put(i,0.);
+            idToY.put(i,0.);
+            idToClus.put(i,0);
+        }
+        
+        int id;
+        for(int i=0;i<ids.size();i++){            
+            id = ids.get(i);
+            idToX.put(id, (xs.get(i) + 5000.)/10000*800);
+            idToY.put(id, (ys.get(i)+ 5000.)/10000*600);
+            idToClus.put(id, clus.get(i));
+        }
+    }
+    
+    public static void readGephi() throws IOException{
+        String sourceFile = "gephi_output.txt";
+        BufferedReader br1 = new BufferedReader(new FileReader(sourceFile));
+        String line = br1.readLine();
+        while(line != null){
+            if(line.contains("<node id")){
+                int a = line.indexOf("\"");
+                int b = line.lastIndexOf("\"");
+                int id = Integer.parseInt(line.substring(a+1,b));
+                ids.add(id-1);
+            }
+            if(line.contains(" for=\"Modularity Class\" ")){
+                int a = line.indexOf("e=\"");
+                int b = line.lastIndexOf("\"");
+                int cid = Integer.parseInt(line.substring(a+3,b));
+                clus.add(cid);
+            }
+            if(line.contains("viz:position")){
+                int sp = line.indexOf("y=");
+                String one = line.substring(0,sp);
+                String two = line.substring(sp+3,line.indexOf("z="));
+                
+                int a = one.indexOf("\"");
+                int b = one.lastIndexOf("\"");
+
+                double x = Double.parseDouble(one.substring(a+1,b));
+                xs.add(x);
+
+                b = two.lastIndexOf("\"");
+                double y = Double.parseDouble(two.substring(0,b));
+                ys.add(y);
+            }
+            
+            line=br1.readLine();
+        }
+        
+    }
+    public static void extract() throws FileNotFoundException, UnsupportedEncodingException, IOException{
         String source = "gear_profiles_old.json";
-        String clustfile = "cluster.txt";
+
         BufferedReader br1 = new BufferedReader(new FileReader(source));
-        BufferedReader br2 = new BufferedReader(new FileReader(clustfile));
+
         PrintWriter wr1 = new PrintWriter("gear_profiles.json", "UTF-8");
+        System.out.println("Extracting");
+        
         String line = br1.readLine();
         String threeElements = "";
+        
+        int currentID = 0;
         while(line!=null){
-            
-            if(line.contains("pos_x")){
-                threeElements = br2.readLine();
-                line = "\"pos_x\"" + ":" + getElement(threeElements, 0);
+                    //System.out.println(line);
+
+            if(line.contains("\"member_id\":")){
+                currentID = Integer.parseInt( line.substring(line.indexOf(":")+1, line.indexOf(",")) );
+                        System.out.println("Extracting");
+
+            }
+            else if(line.contains("pos_x")){
+                line = "\"pos_x\"" + ":" + idToX.get(currentID);
             }
             else if(line.contains("pos_y")){
-                line = "\"pos_y\"" + ":" + getElement(threeElements, 1);
+                line = "\"pos_y\"" + ":" + idToY.get(currentID);
             }
             else if(line.contains("cluster_id")){
-                line = "\"cluster_id\"" + ":" + getElement(threeElements, 2);
+                line = "\"cluster_id\"" + ":" + idToClus.get(currentID);
             }
             
             wr1.println(line);
